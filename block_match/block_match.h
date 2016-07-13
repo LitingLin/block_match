7e9c2c295814da444eb73ec10434d1843e478d52
liting <linliting06@live.com>
2016-07-14 04:02:30 +0800

    c

--- block_match/block_match.h
+++ block_match/block_match.h
@@ -1,27 +1,27 @@
 #pragma once
 
 #ifdef __cplusplus
-#define EXPORT extern "C"
+#define LIB_BLOCK_MATCH_EXPORT extern "C"
 #else
-#define EXPORT
+#define LIB_BLOCK_MATCH_EXPORT
 #endif
 
 #include <stdbool.h>
 #include <stddef.h>
 
 enum Method { MSE, CC };
-EXPORT
+LIB_BLOCK_MATCH_EXPORT
 bool initialize(void **instance, size_t matA_M, size_t matA_N, size_t matB_M, size_t matB_N, size_t block_M, size_t block_N, size_t neighbour_M, size_t neighbour_N, size_t stride_M, size_t stride_N);
-EXPORT
+LIB_BLOCK_MATCH_EXPORT
 bool process(void *instance, float *matA, float *matB, enum Method method);
-EXPORT
+LIB_BLOCK_MATCH_EXPORT
 void getResult(void *instance, float **result, size_t *result_dim0, size_t *result_dim1, size_t *result_dim2, size_t *result_dim3);
-EXPORT
+LIB_BLOCK_MATCH_EXPORT
 void finalize(void *instance);
-EXPORT
+LIB_BLOCK_MATCH_EXPORT
 bool reset();
 
-EXPORT
+LIB_BLOCK_MATCH_EXPORT
 void onLoad();
-EXPORT
+LIB_BLOCK_MATCH_EXPORT
 void atExit();
